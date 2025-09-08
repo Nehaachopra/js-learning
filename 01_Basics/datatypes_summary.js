7f40e85ec41e2d256f1d178ee67337545b6c82ab
Nehaachopra <140242912+Nehaachopra@users.noreply.github.com>
2025-09-08 16:30:48 +0000

    datatypes summary

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,14 @@
+// there are 2 types of datatypes - primitives and non primitives
+// primitives - string, number, boolean, null, undefined, symbol, bigInt
+// non-primitive or references - arrays, objects, functions
+
+// JS is dynamic language (because you do not have to define the datatype of variabe!!)
+
+//datatype => typeof
+//number=>number
+//null=>object
+//undefined=> undefined
+//symbol=> symbol
+//array=> object
+// function=> function
+//object=> object

0e51cb5d59ab0c96719121073b4e79c256b24f7c
Nehaachopra <140242912+Nehaachopra@users.noreply.github.com>
2025-09-08 14:02:06 +0000

    increment postfix and prefix

--- 01_Basics/03_Conversions.js
+++ 01_Basics/03_Conversions.js
@@ -17,3 +17,20 @@ console.log(valueInBoolean);
 
 let valueInString = String(score);
 console.log(valueInString);
+
+let value = 3;
+let negValue = -value;
+//If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+
+//If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"

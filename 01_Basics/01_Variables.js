4345b8713a58c0299878857dbf1676dc6b518038
Nehaachopra <140242912+Nehaachopra@users.noreply.github.com>
2025-09-08 12:09:21 +0000

    datatypes

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "neha@google.com"
+var accountPassword = "12345"
+let accountCity = "Hiriyur"
+let accountState;
+
+// accountId = 111111  Not Alowed
+/* var had block & functional scope problems and hence outdated and replaced by let
+*/
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword])
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hell0"); // we are using nodejs and not browser!
+console.log(23); 
+console.log(45)
+
+let name = "Neha"
+let age = 18
+let isLoggedIn = false
+// null => standalone value // it is an object
+// undefined => 

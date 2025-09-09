2b0cfef230c922c330c6b77e69344c31f37d1f4b
Nehaachopra <140242912+Nehaachopra@users.noreply.github.com>
2025-09-09 05:02:06 +0000

    numbers

--- 01_Basics/06_Numbers.js
+++ 01_Basics/06_Numbers.js
@@ -1,1 +1,28 @@
 const score = 400;
+
+const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); //100.00
+
+const otherNumber = 1123.8966666;
+// console.log(otherNumber.toPrecision(3));
+// 23.89666=>23.9
+//123.89666 => 124
+//  1123.89966 => 1.12e+..
+
+const hundreds =  1000000
+// console.log(hundreds.toLocaleString("en-IN"));
+
+
+// ++++++++++++ Maths ++++++++++
+//math.abs
+//math.round
+//math.floor
+//math.ceil
+//math.min
+//math.max
+//math.random - vaues between 0, 1
+
+const min = 10
+const max = 20

2c181e26a506b8067b4d0cd63eff718b3e473fe6
Nehaachopra <140242912+Nehaachopra@users.noreply.github.com>
2025-09-09 03:31:26 +0000

    summary of datatypes

--- 01_Basics/datatypes_summary.js
+++ 01_Basics/datatypes_summary.js
@@ -12,3 +12,26 @@
 //array=> object
 // function=> function
 //object=> object
+
+
+// primitives use stack memory
+// non-primitives use heap memory
+
+//stack memory working(call by value)
+let oneName = "Neha"
+let anotherName = oneName;
+anotherName = "Swayam"
+
+console.log(oneName); //Neha
+console.log(anotherName); //Swayam
+
+
+//heap memory working(call by reference)
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+let userTwo = userOne;
+userTwo.upi = "userTwo@ybl"
+
+console.log(userOne.upi); // userTwo@ybl
